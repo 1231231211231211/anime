6128ac6e75d597f864ca1ab86f87193845a5c945
adachi <g023c1256@g.neec.ac.jp>
2025-01-10 12:09:32 +0900

    ああああ

--- script.js
+++ script.js
@@ -406,6 +406,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
   ];
+  //メニュー
+  const MenuContainer = document.querySelector('.menu_container');
+  const MenuOpen = document.querySelector('.menu_open');
+  const MenuClose = document.querySelector('.menu_close');
+  
+  const SPMenu = document.querySelector('.sample_sp_menu');			
+      MenuContainer.addEventListener('click', () => {
+          MenuOpen.classList.toggle('active');
+          MenuClose.classList.toggle('active');
+          SPMenu.classList.toggle('active');
+      });
+
+
+//アニメ
   const searchForm = document.getElementById("anime-search-form");
   const searchInput = document.getElementById("anime-title");
   const searchResults = document.getElementById("search-results");
